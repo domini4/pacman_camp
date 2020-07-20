ea3b9d55aca28970d3411eadf9b22b7825a8a87e
James Dominic <domini4@clemson.edu>
2020-07-20 15:00:39 -0400

    Camp final version

--- main.ts
+++ main.ts
@@ -2,7 +2,8 @@ enum ActionKind {
     Walking,
     Idle,
     Jumping,
-    Floating
+    Floating,
+    Scared
 }
 function animatePacman () {
     animation.runImageAnimation(
@@ -122,8 +123,8 @@ function clydeResponse () {
     if (!(scene.isTileAWallAt(scene.getCoordinateNTilesAwayFromTile(1, TravelDirection.Left, Clyde)))) {
         ClydePossibleDirections.push(Math.mod(sprites.heading(Clyde) - 90, 360))
     }
-    if (!(scene.isTileAWallAt(scene.getCoordinateNTilesAwayFromTile(1, TravelDirection.Behind, Clyde)))) {
-        ClydePossibleDirections.push(Math.mod(sprites.heading(Clyde) + 180, 360))
+    if (!(scene.isTileAWallAt(scene.getCoordinateNTilesAwayFromTile(1, TravelDirection.Ahead, Clyde)))) {
+        ClydePossibleDirections.push(sprites.heading(Clyde))
     }
     clydeVelocity(arrays.choose(ClydePossibleDirections))
 }
@@ -138,6 +139,9 @@ game.onGameUpdateWithHeading(function () {
     if (scene.spriteContainedWithinTile(Clyde)) {
         clydeMovement()
     }
+    if (Clyde.vx == 0 && Clyde.vy == 0) {
+        clydeVelocity(randint(0, 3) * 90)
+    }
 })
 function animateClyde () {
     animClyde = animation.createAnimation(ActionKind.Floating, 100)
@@ -146,96 +150,194 @@ function animateClyde () {
         . . . . . 4 4 4 4 . . . . . . . 
         . . . 4 4 4 4 4 4 4 4 . . . . . 
         . . . 4 4 4 4 4 4 4 4 4 . . . . 
-        . . 4 4 1 1 4 4 4 4 1 1 4 . . . 
-        . 4 4 1 1 1 1 4 4 1 1 1 1 . . . 
-        . 4 4 8 8 1 1 4 4 8 8 1 1 . . . 
-        . 4 4 8 8 1 1 4 4 8 8 1 1 4 . . 
-        4 4 4 4 1 1 4 4 4 4 1 1 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 . 4 4 4 . . 4 4 4 . 4 4 . . 
-        4 . . . 4 . . . . 4 . . . 4 . . 
+        . . 4 4 1 1 4 4 4 4 1 1 4 4 . . 
+        . 4 4 1 1 1 1 4 4 1 1 1 1 4 4 . 
+        . 4 4 8 8 1 1 4 4 8 8 1 1 4 4 . 
+        . 4 4 8 8 1 1 4 4 8 8 1 1 4 4 4 
+        4 4 4 4 1 1 4 4 4 4 1 1 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 . 4 4 4 . . 4 4 4 . 4 4 4 4 
+        4 . . . 4 . . . . 4 . . . 4 4 4 
         `)
     animClyde.addAnimationFrame(img`
         . . . . . 4 4 4 4 . . . . . . . 
         . . . 4 4 4 4 4 4 4 4 . . . . . 
         . . . 4 4 4 4 4 4 4 4 4 . . . . 
-        . . 4 4 1 1 4 4 4 4 1 1 4 . . . 
-        . 4 4 1 1 1 1 4 4 1 1 1 1 . . . 
-        . 4 4 1 8 8 1 4 4 1 8 8 1 . . . 
-        . 4 4 1 8 8 1 4 4 1 8 8 1 4 . . 
-        4 4 4 4 1 1 4 4 4 4 1 1 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 . . 4 4 4 . 4 4 4 . . 4 . . 
-        4 . . . . 4 . . . 4 . . . . . . 
+        . . 4 4 1 1 4 4 4 4 1 1 4 4 . . 
+        . 4 4 1 1 1 1 4 4 1 1 1 1 4 4 . 
+        . 4 4 1 8 8 1 4 4 1 8 8 1 4 4 . 
+        . 4 4 1 8 8 1 4 4 1 8 8 1 4 4 4 
+        4 4 4 4 1 1 4 4 4 4 1 1 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 . . 4 4 4 . 4 4 4 . . 4 4 4 
+        4 . . . . 4 . . . 4 . . . . 4 4 
         `)
     animClyde.addAnimationFrame(img`
         . . . . . 4 4 4 4 . . . . . . . 
         . . . 4 4 4 4 4 4 4 4 . . . . . 
         . . . 4 4 4 4 4 4 4 4 4 . . . . 
-        . . 4 4 1 1 4 4 4 4 1 1 4 . . . 
-        . 4 4 1 1 1 1 4 4 1 1 1 1 . . . 
-        . 4 4 1 1 8 8 4 4 1 1 8 8 . . . 
-        . 4 4 1 1 8 8 4 4 1 1 8 8 4 . . 
-        4 4 4 4 1 1 4 4 4 4 1 1 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 . 4 4 4 . . 4 4 4 . 4 4 . . 
-        4 . . . 4 . . . . 4 . . . 4 . . 
+        . . 4 4 1 1 4 4 4 4 1 1 4 4 . . 
+        . 4 4 1 1 1 1 4 4 1 1 1 1 4 4 . 
+        . 4 4 1 1 8 8 4 4 1 1 8 8 4 4 . 
+        . 4 4 1 1 8 8 4 4 1 1 8 8 4 4 4 
+        4 4 4 4 1 1 4 4 4 4 1 1 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 . 4 4 4 . . 4 4 4 . 4 4 4 4 
+        4 . . . 4 . . . . 4 . . . 4 4 4 
         `)
     animClyde.addAnimationFrame(img`
         . . . . . 4 4 4 4 . . . . . . . 
         . . . 4 4 4 4 4 4 4 4 . . . . . 
         . . . 4 4 4 4 4 4 4 4 4 . . . . 
-        . . 4 4 1 1 4 4 4 4 1 1 4 . . . 
-        . 4 4 1 1 1 1 4 4 1 1 1 1 . . . 
-        . 4 4 1 8 8 1 4 4 1 8 8 1 . . . 
-        . 4 4 1 8 8 1 4 4 1 8 8 1 4 . . 
-        4 4 4 4 1 1 4 4 4 4 1 1 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 . . 4 4 4 . 4 4 4 . . 4 . . 
-        4 . . . . 4 . . . 4 . . . . . . 
+        . . 4 4 1 1 4 4 4 4 1 1 4 4 . . 
+        . 4 4 1 1 1 1 4 4 1 1 1 1 4 4 . 
+        . 4 4 1 8 8 1 4 4 1 8 8 1 4 4 . 
+        . 4 4 1 8 8 1 4 4 1 8 8 1 4 4 4 
+        4 4 4 4 1 1 4 4 4 4 1 1 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 . . 4 4 4 . 4 4 4 . . 4 4 4 
+        4 . . . . 4 . . . 4 . . . . 4 4 
         `)
     animClyde.addAnimationFrame(img`
         . . . . . 4 4 4 4 . . . . . . . 
         . . . 4 4 4 4 4 4 4 4 . . . . . 
         . . . 4 4 4 4 4 4 4 4 4 . . . . 
-        . . 4 4 1 1 4 4 4 4 1 1 4 . . . 
-        . 4 4 1 1 1 1 4 4 1 1 1 1 . . . 
-        . 4 4 8 8 1 1 4 4 8 8 1 1 . . . 
-        . 4 4 8 8 1 1 4 4 8 8 1 1 4 . . 
-        4 4 4 4 1 1 4 4 4 4 1 1 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        4 4 . 4 4 4 . . 4 4 4 . 4 4 . . 
-        4 . . . 4 . . . . 4 . . . 4 . . 
+        . . 4 4 1 1 4 4 4 4 1 1 4 4 . . 
+        . 4 4 1 1 1 1 4 4 1 1 1 1 4 4 . 
+        . 4 4 8 8 1 1 4 4 8 8 1 1 4 4 . 
+        . 4 4 8 8 1 1 4 4 8 8 1 1 4 4 4 
+        4 4 4 4 1 1 4 4 4 4 1 1 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+        4 4 . 4 4 4 . . 4 4 4 . 4 4 4 4 
+        4 . . . 4 . . . . 4 . . . 4 4 4 
+        `)
+    animClyde = animation.createAnimation(ActionKind.Scared, 100)
+    animation.attachAnimation(Clyde, animClyde)
+    animClyde.addAnimationFrame(img`
+        . . . . . 8 8 8 8 . . . . . . . 
+        . . . 8 8 8 8 8 8 8 8 . . . . . 
+        . . . 8 8 8 8 8 8 8 8 8 . . . . 
+        . . 8 8 1 1 8 8 8 8 1 1 8 8 . . 
+        . 8 8 1 1 1 1 8 8 1 1 1 1 8 8 . 
+        . 8 8 d d 1 1 8 8 d d 1 1 8 8 . 
+        . 8 8 d d 1 1 8 8 d d 1 1 8 8 8 
+        8 8 8 8 1 1 8 8 8 8 1 1 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 d d 8 8 d d 8 8 d d 8 8 d d 8 
+        d 8 8 d d 8 8 d d 8 8 d d 8 8 d 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 . 8 8 8 . . 8 8 8 . 8 8 8 8 
+        8 . . . 8 . . . . 8 . . . 8 8 8 
+        `)
+    animClyde.addAnimationFrame(img`
+        . . . . . 8 8 8 8 . . . . . . . 
+        . . . 8 8 8 8 8 8 8 8 . . . . . 
+        . . . 8 8 8 8 8 8 8 8 8 . . . . 
+        . . 8 8 1 1 8 8 8 8 1 1 8 8 . . 
+        . 8 8 1 1 1 1 8 8 1 1 1 1 8 8 . 
+        . 8 8 1 d d 1 8 8 1 d d 1 8 8 . 
+        . 8 8 1 d d 1 8 8 1 d d 1 8 8 8 
+        8 8 8 8 1 1 8 8 8 8 1 1 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 d d 8 8 d d 8 8 d d 8 8 d d 
+        d d 8 8 d d 8 8 d d 8 8 d d 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 . . 8 8 8 . 8 8 8 . . 8 8 8 
+        8 . . . . 8 . . . 8 . . . . 8 8 
+        `)
+    animClyde.addAnimationFrame(img`
+        . . . . . 8 8 8 8 . . . . . . . 
+        . . . 8 8 8 8 8 8 8 8 . . . . . 
+        . . . 8 8 8 8 8 8 8 8 8 . . . . 
+        . . 8 8 1 1 8 8 8 8 1 1 8 8 . . 
+        . 8 8 1 1 1 1 8 8 1 1 1 1 8 8 . 
+        . 8 8 1 1 d d 8 8 1 1 d d 8 8 . 
+        . 8 8 1 1 d d 8 8 1 1 d d 8 8 8 
+        8 8 8 8 1 1 8 8 8 8 1 1 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        d 8 8 d d 8 8 d d 8 8 d d 8 8 d 
+        8 d d 8 8 d d 8 8 d d 8 8 d d 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 . 8 8 8 . . 8 8 8 . 8 8 8 8 
+        8 . . . 8 . . . . 8 . . . 8 8 8 
+        `)
+    animClyde.addAnimationFrame(img`
+        . . . . . 8 8 8 8 . . . . . . . 
+        . . . 8 8 8 8 8 8 8 8 . . . . . 
+        . . . 8 8 8 8 8 8 8 8 8 . . . . 
+        . . 8 8 1 1 8 8 8 8 1 1 8 8 . . 
+        . 8 8 1 1 1 1 8 8 1 1 1 1 8 8 . 
+        . 8 8 1 d d 1 8 8 1 d d 1 8 8 . 
+        . 8 8 1 d d 1 8 8 1 d d 1 8 8 8 
+        8 8 8 8 1 1 8 8 8 8 1 1 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 d d 8 8 d d 8 8 d d 8 8 d d 
+        d d 8 8 d d 8 8 d d 8 8 d d 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 . . 8 8 8 . 8 8 8 . . 8 8 8 
+        8 . . . . 8 . . . 8 . . . . 8 8 
+        `)
+    animClyde.addAnimationFrame(img`
+        . . . . . 8 8 8 8 . . . . . . . 
+        . . . 8 8 8 8 8 8 8 8 . . . . . 
+        . . . 8 8 8 8 8 8 8 8 8 . . . . 
+        . . 8 8 1 1 8 8 8 8 1 1 8 8 . . 
+        . 8 8 1 1 1 1 8 8 1 1 1 1 8 8 . 
+        . 8 8 d d 1 1 8 8 d d 1 1 8 8 . 
+        . 8 8 d d 1 1 8 8 d d 1 1 8 8 8 
+        8 8 8 8 1 1 8 8 8 8 1 1 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 d d 8 8 d d 8 8 d d 8 8 d d 8 
+        d 8 8 d d 8 8 d d 8 8 d d 8 8 d 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+        8 8 . 8 8 8 . . 8 8 8 . 8 8 8 8 
+        8 . . . 8 . . . . 8 . . . 8 8 8 
         `)
 }
+info.onCountdownEnd(function () {
+    clydeScared = 0
+    animation.setAction(Clyde, ActionKind.Floating)
+})
 function clydeMovement () {
-    if (scene.isTileAWallAt(scene.getCoordinateNTilesAwayFromTile(1, TravelDirection.Ahead, Clyde))) {
+    if (scene.getTileColCoordinate(scene.getTileLocationOfSprite(Clyde)) != clydePrevCol || scene.getTileRowCoordinate(scene.getTileLocationOfSprite(Clyde)) != clydePrevRow) {
         clydeResponse()
+        clydePrevRow = scene.getTileRowCoordinate(scene.getTileLocationOfSprite(Clyde))
+        clydePrevCol = scene.getTileColCoordinate(scene.getTileLocationOfSprite(Clyde))
     }
 }
 scene.onOverlapTile(SpriteKind.Player, myTiles.tile5, function (sprite, location) {
@@ -243,11 +345,30 @@ scene.onOverlapTile(SpriteKind.Player, myTiles.tile5, function (sprite, location
     music.powerUp.play()
     info.changeScoreBy(10)
     Pellet_Count += -1
+    clydeScared = 1
+    animation.setAction(Clyde, ActionKind.Scared)
+    info.startCountdown(30)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    if (clydeScared == 1) {
+        info.changeScoreBy(100)
+        music.powerUp.play()
+        tiles.placeOnTile(Clyde, tiles.getTileLocation(5, 3))
+    } else {
+        info.changeLifeBy(-1)
+        music.wawawawaa.play()
+        tiles.placeOnTile(Pacman, tiles.getTileLocation(1, 4))
+        tiles.placeOnTile(Clyde, tiles.getTileLocation(5, 3))
+    }
 })
 let animClyde: animation.Animation = null
 let ClydePossibleDirections: number[] = []
+let clydeScared = 0
+let clydePrevRow = 0
+let clydePrevCol = 0
 let Clyde: Sprite = null
 let Pacman: Sprite = null
+music.setVolume(20)
 info.setScore(0)
 tiles.setTilemap(tiles.createTilemap(hex`1000100001010101010101010101010101010101010302020202020202020202020203010102010101020101010102010101020101020202020201010101020202020201010201010102010101010201010102010102020202020202020202020202020101010101010201010101020101010101020202020202010000010202020202020101010101020101010102010101010101020202020202020202020202020201010201010102010101010201010102010102020202020202020202020202020101020102010201010101020102010201010201020102010101010201020102010103020202020202020202020202030101010101010101010101010101010101`, img`
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
@@ -286,6 +407,7 @@ Pacman = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 tiles.placeOnTile(Pacman, tiles.getTileLocation(1, 4))
+info.setLife(1)
 let Pellet_Count = tiles.getTilesByType(myTiles.tile4).length + tiles.getTilesByType(myTiles.tile5).length
 animatePacman()
 Clyde = sprites.create(img`
@@ -308,8 +430,11 @@ Clyde = sprites.create(img`
     `, SpriteKind.Enemy)
 tiles.placeOnTile(Clyde, tiles.getTileLocation(5, 3))
 Clyde.setVelocity(0, -50)
+clydePrevCol = scene.getTileColCoordinate(scene.getTileLocationOfSprite(Clyde))
+clydePrevRow = scene.getTileRowCoordinate(scene.getTileLocationOfSprite(Clyde))
 animateClyde()
 animation.setAction(Clyde, ActionKind.Floating)
+clydeScared = 0
 game.onUpdateInterval(500, function () {
     if (Pellet_Count == 0) {
         game.over(true, effects.confetti)
